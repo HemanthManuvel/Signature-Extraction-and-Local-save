7bfb262d620f4c1d1cbeb63364456b8daf3031b9
HemanthManuvel <115690910+HemanthManuvel@users.noreply.github.com>
2023-09-13 19:49:59 +0530

    signature_extractor.py

--- signature_extractor_Complete.py
+++ signature_extractor_Complete.py
@@ -8,7 +8,8 @@ constant_parameter_1 = 84
 constant_parameter_2 = 250
 constant_parameter_3 = 100
 constant_parameter_4 = 18
-img = cv2.imread('H:\MINI-PROJECT/INPUT\IMAGES/Demo.jpg', 0)
+//Input area
+img = cv2.imread('Demo.jpg', 0)
 img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]  
 blobs = img > img.mean()
 blobs_labels = measure.label(blobs, background=1)
@@ -38,7 +39,9 @@ component_sizes = np.bincount(pr_version.ravel())
 too_small = component_sizes > (a4_big_size_outliar_constant)
 too_small_mask = too_small[pr_version]
 pr_version[too_small_mask] = 0
-plt.imsave('H:\MINI-PROJECT\pr_version.png', pr_version)
-img = cv2.imread('H:\MINI-PROJECT\pr_version.png', 0)
+//previous area
+plt.imsave('pr_version.png', pr_version)
+img = cv2.imread('pr_version.png', 0)
 img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-cv2.imwrite("H:\MINI-PROJECT\OUTPUT\SIGNATURE/output.png", img)
+//final output area
+cv2.imwrite("output.png", img)
